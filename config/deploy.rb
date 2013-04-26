bbbebd9ee944385e5e5cd39896760186387efcfe
Dmtiriy Budnik <dmitriy.budnik+github@gmail.com>
2013-04-26 16:45:08 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,34 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require "bundler/capistrano" 
+set :application, 'start'
+set :location, 'start'
+set :repository,  :git
+set :scm, :git
+set :repository,  "."
+set :deploy_via, :copy
+set :user, "mechanic"
+set :deploy_to, "/var/www/#{application}"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+server "48.ruby.ug", :app, :web, :db, :primary => true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+namespace :deploy do
+  desc "Zero-downtime restart of Unicorn"
+  task :restart, :except => { :no_release => true } do
+    run "kill -s USR2 `cat /var/www/#{application}/shared/pids/unicorn.pid`"
+  end
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+  desc "Start unicorn"
+  task :start, :except => { :no_release => true } do
+    #run "sudo sv start #{application}"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec unicorn_rails -c config/unicorn.rb -D"
+  end
+
+  desc "Stop unicorn"
+  task :stop, :except => { :no_release => true } do
+    #run "sudo sv stop #{application}"
+    run "kill -s QUIT `cat /var/www/#{application}/shared/pids/unicorn.pid`"
+  end
+end
