dee474a5d1c915b0e63d98cd2a97438e55895f8a
Dmtiriy Budnik <dmitriy.budnik+github@gmail.com>
2013-04-28 15:06:25 +0300

    login to apply

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,8 +1,14 @@
 #encoding: utf-8
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def failure
-    redirect_to :root, alert: "Невдалось авторизуватись через facebook"
-  end  
+    flash[:alert] = "Невдалось авторизуватись через facebook"
+
+    if request.env['omniauth.params']['activity'].present?
+      redirect_to ::Activity.find request.env['omniauth.params']['activity']
+    else
+      redirect_to :root 
+    end
+  end
 
   def facebook
     @volunteer = Volunteer.find_or_create_by_uid request.env["omniauth.auth"]["uid"] do |v|
@@ -12,8 +18,13 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
 
     if @volunteer.persisted?
-      sign_in_and_redirect @volunteer, :event => :authentication #this will throw if @volunteer is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      if request.env['omniauth.params']['activity'].present?
+        sign_in @volunteer
+        redirect_to ::Activity.find request.env['omniauth.params']['activity']
+      else
+        sign_in_and_redirect @volunteer, :event => :authentication #this will throw if @volunteer is not activated
+        set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      end
     else
       #would not get here
       session["devise.facebook_data"] = request.env["omniauth.auth"]
