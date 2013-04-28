28ab1a66726cbaa92910817bd6a12924e8f3ea2f
Dmtiriy Budnik <dmitriy.budnik+github@gmail.com>
2013-04-28 14:55:05 +0300

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-{meds: 'twirl#hospitalIcon',
- food: 'twirl#shopIcon',
- household: 'twirl#workshopIcon',
- lift: 'twirl#carIcon'}.each_pair do |c, i|
+{meds: 'twirl#hospitalIcon', food: 'twirl#shopIcon',
+ household: 'twirl#workshopIcon', lift: 'twirl#carIcon'}.each_pair do |c, i|
   Category.create name: c, icon: i
 end
 
@@ -21,7 +19,6 @@ Person.create 10.times.map{{name: Vydumschik::Name.full_name,
   }}
 Person.all.map{|p| p.address='Киев '+Vydumschik::Address.street_address; p.save}
   
-puts Activity.count
 require 'json'
 json = []
 json << '{"category_id":2,"created_at":"2013-04-27T21:27:34Z","deadline":"2013-04-30","description":"фЫВФЫ","name":"фы","person_id":7,"state":"inprogress","updated_at":"2013-04-28T06:55:34Z","volunteer_id":4}'
@@ -30,7 +27,6 @@ json << '{"category_id":3,"created_at":"2013-04-28T07:18:04Z","deadline":"2013-0
 json << '{"category_id":4,"created_at":"2013-04-28T07:44:46Z","deadline":"2013-04-30","description":"Подвезти детали","name":"Подвезти","person_id":2,"state":"open","updated_at":"2013-04-28T07:44:46Z","volunteer_id":null}'
 json << '{"category_id":2,"created_at":"2013-04-28T07:18:04Z","deadline":"2013-04-27","description":"Help in the house to guy that cannot do this himself","name":"Help in the house","person_id":7,"state":"inprogress","updated_at":"2013-04-28T07:44:09Z","volunteer_id":4}'
 json << '{"category_id":2,"created_at":"2013-04-28T07:18:04Z","deadline":"2013-04-27","description":"Help in the house to guy that cannot do this himself","name":"Help in the house","person_id":1,"state":"open","updated_at":"2013-04-28T07:44:09Z","volunteer_id":4}'
-json << '{"category_id":1,"created_at":"2013-04-28T07:44:46Z","deadline":"2013-04-30","description":"Подвезти детали","name":"Подвезти","person_id":5,"state":"open","updated_at":"2013-04-28T07:44:46Z","volunteer_id":null}'
+json << '{"category_id":1,"created_at":"2013-04-28T07:44:46Z","deadline":"2013-04-30","description":"Подв езти детали","name":"Подвезти","person_id":5,"state":"open","updated_at":"2013-04-28T07:44:46Z","volunteer_id":null}'
 json.each {|j| Activity.new.update_attributes( (JSON.parse(j)), without_protection: true)}
-puts Activity.count
 
