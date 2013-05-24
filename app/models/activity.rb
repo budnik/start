85dc37cb361d39f7cb16b76cb5155fc857e240b0
Dmtiriy Budnik <dmitriy.budnik+github@gmail.com>
2013-05-25 00:04:34 +0300

    admin can mark activity finished

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,12 +2,17 @@ class Activity < ActiveRecord::Base
   belongs_to :person, class_name: Person
   belongs_to :volunteer
   belongs_to :category
-  attr_accessible :name, :deadline, :description, :state, :person_id, :category_id
+  attr_accessible :name, :deadline, :description, :state, :person_id, :category_id, :state
 
   state_machine :state, :initial => :open do
     event :apply do
       transition :open => :inprogress
     end
+
+    event :finish do
+      transition any => :done
+    end
+
   end 
 
   rails_admin do
@@ -16,6 +21,8 @@ class Activity < ActiveRecord::Base
     configure :description, :text
     configure :deadline, :date
     configure :category, :belongs_to_association
+    configure :volunteer do; read_only true end
+
     configure :state, :enum do
       enum do
         %w[open inprogress done]
